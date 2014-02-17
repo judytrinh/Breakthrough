b778b970cad65b876815ad71a6a56ab3d0bdc244
Judy Trinh <jtrinh74@gmail.com>
2014-02-16 21:45:05 -0500

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+	
+	public Vector3 VELOCITY;
+	
+	void Start () {
+		VELOCITY = new Vector3 (3.0f, 3.0f, 0.0f);
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name == "Wall")
+			VELOCITY.x *= -1;
+		else if (col.gameObject.name == "Ceiling")
+			VELOCITY.y *= -1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(VELOCITY * Time.deltaTime);
+	}
+}
---
+++ Assets/GlobalController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalController : MonoBehaviour {
+
+	// initialize here to edit in Inspector later
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
