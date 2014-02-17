a37a962deca664eb3fd13d00cbbacb53b8d50edb
Judy Trinh <jtrinh74@gmail.com>
2014-02-16 23:15:01 -0500

    Move paddle with cursor

---
+++ Assets/PaddleController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 wantedPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.z, mousePos.z));
+		transform.position = new Vector3(wantedPos.x, transform.position.y, transform.position.z);
+	}
+}
