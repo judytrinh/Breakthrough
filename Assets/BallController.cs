9610ebf0b69d9ab43b81bc22f9780c843a24057c
Judy Trinh <jtrinh74@gmail.com>
2014-02-18 02:28:09 -0500

    Moved the brick prefab generation to the right place, global tracking vars and such

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,26 +6,8 @@ public class BallController : MonoBehaviour {
 
 	public Vector3 _velocity;
 
-	private int BRICK_NUM_COLS = 12;
-	private int BRICK_NUM_ROWS = 6;
-	private float BRICK_WIDTH_BUFFER_SPACE = 0.05f;
-	private float BRICK_HEIGHT_BUFFER_SPACE = 0.05f;
-
 	void Start () {
 		_velocity = new Vector3(3.0f, 3.0f, 0.0f);
-
-		for (int i = 0; i < BRICK_NUM_COLS; i++) {
-			for (int j = 0; j < BRICK_NUM_ROWS; j++) {
-				GameObject brick = Instantiate(Resources.Load("BrickPrefab")) as GameObject;
-				BoxCollider mainCollider = brick.GetComponent<BoxCollider>();
-				float brickWidth = brick.transform.localScale.x * mainCollider.size.x;
-				float brickHeight = brick.transform.localScale.y * mainCollider.size.y;
-				Vector3 pos = new Vector3(5.4f, 8.7f, 0.0f);
-				pos.x -= (brickWidth + BRICK_WIDTH_BUFFER_SPACE) * i;
-				pos.y -= (brickHeight + BRICK_HEIGHT_BUFFER_SPACE) * j;
-				brick.transform.position = pos;
-			}
-		}
 	}
 
 	void OnCollisionEnter(Collision col) {
--- Assets/GlobalController.cs
+++ Assets/GlobalController.cs
@@ -3,10 +3,37 @@ using System.Collections;
 
 public class GlobalController : MonoBehaviour {
 
-	// initialize here to edit in Inspector later
+	private int _score;
+	private int _livesLeft;
+	private int _bricksRemaining;
+	public bool _paused;
+
+	private int BRICK_NUM_COLS = 12;
+	private int BRICK_NUM_ROWS = 6;
+	private float BRICK_WIDTH_BUFFER_SPACE = 0.05f;
+	private float BRICK_HEIGHT_BUFFER_SPACE = 0.05f;
 
 	void Start () {
-	
+		_score = 0;
+		_livesLeft = 3;
+
+		// generate BrickPrefabs
+		for (int i = 0; i < BRICK_NUM_COLS; i++) {
+			for (int j = 0; j < BRICK_NUM_ROWS; j++) {
+
+				GameObject brick = Instantiate(Resources.Load("BrickPrefab")) as GameObject;
+				BoxCollider mainCollider = brick.GetComponent<BoxCollider>();
+
+				float brickWidth = brick.transform.localScale.x * mainCollider.size.x;
+				float brickHeight = brick.transform.localScale.y * mainCollider.size.y;
+
+				Vector3 pos = new Vector3(5.4f, 8.7f, 0.0f);
+				pos.x -= (brickWidth + BRICK_WIDTH_BUFFER_SPACE) * i;
+				pos.y -= (brickHeight + BRICK_HEIGHT_BUFFER_SPACE) * j;
+
+				brick.transform.position = pos;
+			}
+		}
 	}
 	
 	// Update is called once per frame
