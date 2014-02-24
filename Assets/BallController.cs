609ada7a34836b2aeacd084d26bc1753b563c831
Judy Trinh <jtrinh74@gmail.com>
2014-02-24 13:50:17 -0500

    Handle floor bounce, refactoring

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,22 +5,49 @@ using System.Collections;
 public class BallController : MonoBehaviour {
 
 	public Vector3 _velocity;
-	private Vector3 RESET_POSITION = new Vector3(0, 2.0f, 0);
+	private GameObject _paddle;
+	private PaddleController _paddleController;
+	private Vector3 RESET_POSITION = new Vector3(0, 5.9f, 0);
 	private Vector3 RESET_VELOCITY = new Vector3(3.0f, 3.0f, 0.0f);
 
 	void Start () {
+		_paddle = GameObject.Find("Paddle");
+		_paddleController = _paddle.GetComponent<PaddleController>();
 		Reset();
 	}
 
 	void OnCollisionEnter(Collision col) {
-		if (col.gameObject.name == "Wall")
-			_velocity.x *= -1;
-		else if (col.gameObject.name == "Ceiling")
-			_velocity.y *= -1;
-		else if (col.gameObject.name == "Paddle")
-			_velocity.y *= -1;
-		else if (col.gameObject.name == "BrickPrefab(Clone)")
-			_velocity.y *= -1;
+		string gameObjName = col.gameObject.name;
+		switch(gameObjName) {
+			case "Wall":
+				_velocity.x *= -1;
+				break;
+			case "Ceiling":
+				_velocity.y *= -1;
+				break;
+			case "Floor":
+				_velocity.y *= -1;
+				break;
+			case "Paddle":
+				if (!_paddleController._invisible) _velocity.y *= -1;
+				break;
+			case "BrickPrefab(Clone)":
+				_velocity.y *= -1;
+				break;
+			default:
+				Debug.Log("UNHANDLED GAME OBJECT IN BallController.cs: OnCollisionEnter()");
+				break;
+		}
+//		if (col.gameObject.name == "Wall")
+//			_velocity.x *= -1;
+//		else if (col.gameObject.name == "Ceiling")
+//			_velocity.y *= -1;
+//		else if (col.gameObject.name == "Paddle") {
+//			if (!_paddleController._invisible) {
+//				_velocity.y *= -1;
+//			}
+//		} else if (col.gameObject.name == "BrickPrefab(Clone)")
+//			_velocity.y *= -1;
 	}
 
 	public void Reset() {
--- Assets/PaddleController.cs
+++ Assets/PaddleController.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class PaddleController : MonoBehaviour {
 
+	public bool _invisible;
 	private GameObject _globalObj;
 	private GlobalController _globalController; 
-	private BoxCollider mainCollider;
+	private BoxCollider _mainCollider;
 
 	// Use this for initialization
 	void Start () {
-		mainCollider = GetComponent<BoxCollider>();
+		_invisible = false;
+		_mainCollider = GetComponent<BoxCollider>();
 		Screen.showCursor = false;
 		_globalObj = GameObject.Find("Global Controller");
 		_globalController = _globalObj.GetComponent<GlobalController>();
@@ -17,24 +19,30 @@ public class PaddleController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		if (!_globalController._mode3D) {
+
 			Vector3 mousePos = Input.mousePosition;
 			Vector3 wantedPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.z, mousePos.z));
-			transform.position = new Vector3(wantedPos.x + mainCollider.size.x/2, transform.position.y, transform.position.z);
+			transform.position = new Vector3(wantedPos.x + _mainCollider.size.x/2, transform.position.y, transform.position.z);
+
 		} else {
 
-			if (Input.GetKey(KeyCode.LeftArrow)) {
-				
+			if (Input.GetKey(KeyCode.LeftArrow))
 				transform.Translate(new Vector3(0.1f, 0, 0));
-				
-			} else if (Input.GetKey(KeyCode.RightArrow)) {
-
+			else if (Input.GetKey(KeyCode.RightArrow))
 				transform.Translate(new Vector3(-0.1f, 0, 0));
-
-			}
 
 			Vector3 mainCamPos = Camera.main.transform.position;
 			Camera.main.transform.position = new Vector3(transform.position.x, mainCamPos.y, mainCamPos.z);
+
+		}
+
+		// Determining pass-through behavior
+		if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+			_invisible = true;
+		} else {
+			_invisible = false;
 		}
 
 	}
